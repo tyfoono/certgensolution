2a85eef54973dd2939f3f2eb8d3eb86dc3c2f1f3
tyfoono <aabzhinova@ya.ru>
2025-11-28 22:11:54 +0300

    all templates created

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ options = {
     'default_css': '@page { size: A4; margin: 0; }',
 }
 
-personal_data = {
-    "name": "NAME",
-    "surname": "SURNAME",
-    "email": "EMAIL",
-    "language": "LANGUAGE",
-    "role": "Призёр",
-    "place": "-"
+personal_info = {
+    "name": "Alex",
+    "surname": "Abzhinov",
+    "email": "email",
+    "language": "en",
+    "role": "Победитель",
+    "place": "3"
 }
 
 event_info = {
-    "name": "TITLE",
-    "date": "DATE"
+    "name": "IT-ROUND",
+    "date": "13.02.2077"
 }
 
 
-def render_pdf(event_info: dict, person_data: dict):
+def render_pdf(event_data: dict, personal_data: dict):
     templates = {
         "Спикер": "appreciation_letter",
         "Победитель": "winner_diploma",
@@ -33,34 +33,34 @@ def render_pdf(event_info: dict, person_data: dict):
     }
 
     if personal_data["role"] != "Победитель":
-        template_name = templates[person_data["role"]]
-    elif person_data["place"] != "-":
-        template_name = "winner_place_diploma"
+        template_name = f"{templates[personal_data['role']]}_{personal_data['language']}"
+    elif personal_data["place"] != "-":
+        template_name = f"winner_place_diploma_{personal_data['language']}"
     else:
-        template_name = "winner_diploma"
+        template_name = f"winner_diploma_{personal_data['language']}"
 
     with open(f"templates/{template_name}.html", 'r', encoding="utf-8") as f:
         template_content = f.read()
     template = Template(template_content)
 
 
-    if template_name == "winner_place_diploma":
-        rendered_html = template.render(event_name=event_info["name"],
-                                        name=person_data["name"],
-                                        surname=person_data["surname"],
-                                        grade=person_data["place"],
-                                        date=event_info["date"])
+    if "winner_place_diploma" in template_name:
+        rendered_html = template.render(event_name=event_data["name"],
+                                        name=personal_data["name"],
+                                        surname=personal_data["surname"],
+                                        grade=personal_data["place"],
+                                        date=event_data["date"])
     else:
-        rendered_html = template.render(event_name=event_info["name"],
-                                        surname=person_data["surname"],
-                                        name=person_data["name"],
-                                        date=event_info["date"])
+        rendered_html = template.render(event_name=event_data["name"],
+                                        surname=personal_data["surname"],
+                                        name=personal_data["name"],
+                                        date=event_data["date"])
 
-    pdf_dir = event_info["name"]
+    pdf_dir = event_data["name"]
     if not os.path.exists(pdf_dir):
         os.makedirs(pdf_dir)
 
-    pdf_path = f"{pdf_dir}/{person_data["name"]}_{person_data["surname"]}.pdf"
+    pdf_path = f"{pdf_dir}/{personal_data["name"]}_{personal_data["surname"]}.pdf"
 
 
     HTML(string=rendered_html, base_url=BASE_URL).write_pdf(pdf_path)
@@ -68,4 +68,4 @@ def render_pdf(event_info: dict, person_data: dict):
     print(f"PDF успешно создан: {pdf_path}")
 
 
-render_pdf(event_info, personal_data)
+render_pdf(event_info, personal_info)
